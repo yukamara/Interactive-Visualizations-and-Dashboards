1546d1438d96be17cb6f39f7bc6c223605649322
Yusufu U Kamara <59753837+yukamara@users.noreply.github.com>
2021-07-15 11:32:38 -0700

    Made small additions to app.js

---
+++ index.js
@@ -0,0 +1,70 @@
+function dropdown() {
+    d3.json("./static/data/samples.json").then(data => {
+        var sampleName = data.names;
+        var dataid = d3.select("#selDataset")
+        sampleName.forEach((sampleid) => {
+            dataid.append("option")
+                .text(sampleid)
+                .property("value", sampleid)
+        })
+        var firstsample = sampleName[0];
+        metadata(firstsample);
+        Charts(firstsample);
+    });
+};
+
+dropdown();
+
+function metadata(id) {
+    d3.json("./static/data/samples.json").then(data => {
+        var sampleMetadata = data.metadata;
+        var result = sampleMetadata.filter(obj => obj.id == id);
+        var filterresult = result[0];
+        var display = d3.select("#sample-metadata");
+        display.html("");
+        Object.entries(filterresult).forEach(([key, value]) => {
+            display.append("h6").text(`${key} ${value}`);
+        });
+
+    });
+}
+
+function Charts(id) {
+    d3.json("./static/data/samples.json").then(data => {
+        var sampledata = data.samples;
+        var result = sampledata.filter(obj => obj.id == id);
+        var filterresult = result[0];
+        var otu_ids = filterresult.otu_ids;
+        var otu_labels = filterresult.otu_labels;
+        var sample_values = filterresult.sample_values;
+        var bubbledata = [{
+            x: otu_ids,
+            y: sample_values,
+            text: otu_labels,
+            mode: "markers",
+            marker: {
+                size: sample_values,
+                color: otu_ids,
+                colorscale: "Earth"
+            }
+        }];
+        var barData = [{
+            y: otu_ids.slice(0, 10).map(val => `OTU ${val}`).reverse(),
+            x: sample_values.slice(0, 10).reverse(),
+            text: otu_labels.slice(0, 10).reverse(),
+            type: "bar",
+            orientation: "h",
+        }];
+        Plotly.newPlot("bar", barData);
+
+        Plotly.newPlot("bubble", bubbledata);
+
+    });
+};
+
+
+function optionChanged(changedid) {
+    metadata(changedid);
+    Charts(changedid);
+    // barCharts(changedid);
+};
--- static/js/app.js
+++ static/js/app.js
@@ -1,10 +1,29 @@
-//const url = "/data/samples.json";
+const url = "./static/data/samples.json";
 
 // Fetch the JSON data and console log it
-d3.json("/data/samples.json").then(function(data) {
-  console.log(data);
+d3.json(url).then(function(data) {
+ console.log(data);
 });
 
 // Promise Pending
 const dataPromise = d3.json(url);
 console.log("Data Promise: ", dataPromise);
+
+
+var unitName = data.names;
+var 
+
+
+// var data = [{
+//   y:['Marc', 'Henrietta', 'Jean', 'Claude', 'Jeffrey', 'Jonathan', 'Jennifer', 'Zacharias'], 
+//     x: [90, 40, 60, 80, 75, 92, 87, 73],
+//     type: 'bar',
+//     orientation: 'h'}]
+
+// var layout = {
+//   title: 'Always Display the Modebar',
+//   showlegend: false}
+
+
+// ***********************************************************************************************************************
+// 
